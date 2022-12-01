70595fa5a3b0c448b735d5d8fb987aa676f018df
user <nhk828@naver.com>
2022-12-01 12:42:23 +0900

    22.12.01

---
+++ 네트워크프로그램/src/com/sist/main/MainClass_Single.java
@@ -0,0 +1,29 @@
+package com.sist.main;
+/*
+ * 		-----------|-----------  =>  동기화 (싱글 쓰레드) ==> 단점 (A가 멈추면 -> B는 수행할 수 없다)
+ * 			 A			 B								   => 데드락 / 스타베이션  ==>  예외처리
+ * 
+ * 		-----------------------
+ * 		 A	 A	 A	 A
+ * 		   B   B   B   B ... => 비동기화 (멀티 쓰레드)
+ */
+public class MainClass_Single {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long start = System.currentTimeMillis();
+		
+		for(int i = 0; i < 100; i++) {
+			System.out.print("★");
+		}
+		long end = System.currentTimeMillis();
+		System.out.println("소요시간 : " + (end - start));
+		
+		for(int i = 0; i < 100; i++) {
+			System.out.print("☆");
+		}
+		end = System.currentTimeMillis();
+		System.out.println("소요시간 : " + (end - start));
+	}
+
+}

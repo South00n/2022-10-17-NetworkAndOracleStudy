f84008d11d3252d1dd90a266dcb6653d4def56e9
user <nhk828@naver.com>
2022-12-13 16:08:21 +0900

    22.12.13

--- FreeBoardProject/src/main/java/com/sist/board/BoardList.java
+++ FreeBoardProject/src/main/java/com/sist/board/BoardList.java
@@ -14,11 +14,11 @@ public class BoardList extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//////////////////////////// 오라클 연결 => 데이터 읽기
-		BoardDAO dao = new BoardDAO();
-		ArrayList<BoardVO> list = dao.boardListDate();
+		BoardDAO dao=new BoardDAO();
+		ArrayList<BoardVO> list=dao.boardListDate();
 		//////////////////////////////////////////////////
 		
-		response.setContentType("text/html;charset=UTF-8");
+		 response.setContentType("text/html;charset=UTF-8");
 		 PrintWriter out=response.getWriter();
 		 
 		 out.println("<html>");
@@ -37,7 +37,7 @@ public class BoardList extends HttpServlet {
 		 out.println("<table class=table>");
 		 out.println("<tr>");
 		 out.println("<td>");
-		 out.println("<a href=\"#\" class=\"btn btn-sm btn-primary\">새글</a>");
+		 out.println("<a href=\"BoardInsert\" class=\"btn btn-sm btn-primary\">새글</a>");
 		 out.println("</td>");
 		 out.println("</tr>");
 		 out.println("</table>");
@@ -50,14 +50,17 @@ public class BoardList extends HttpServlet {
 		 out.println("<th class=text-center width=10%>조회수</th>");
 		 out.println("</tr>");
 		 ////////////////////////////////////// Data 출력 위치
-		 for(BoardVO vo : list) {
+		 for(BoardVO vo:list)
+		 {
+			 
 			 out.println("<tr>");
-			 out.println("<td class=text-center width=10%>" + vo.getNo() + "</td>");
-			 out.println("<td width=45%>" + vo.getSubject() + "</td>");
-			 out.println("<td class=text-center width=15%>" + vo.getName() + "</td>");
-			 out.println("<td class=text-center width=20%>" + vo.getDbday() + "</td>");
-			 out.println("<td class=text-center width=10%>" + vo.getHit() + "</td>");
+			 out.println("<td class=text-center width=10%>"+vo.getNo()+"</td>");
+			 out.println("<td width=45%><a href=BoardDetail?no="+vo.getNo()+">"+vo.getSubject()+"</a></td>");
+			 out.println("<td class=text-center width=15%>"+vo.getName()+"</td>");
+			 out.println("<td class=text-center width=20%>"+vo.getDbday()+"</td>");
+			 out.println("<td class=text-center width=10%>"+vo.getHit()+"</td>");
 			 out.println("</tr>");
+			 
 		 }
 		 //////////////////////////////////////
 		 out.println("</table>");
